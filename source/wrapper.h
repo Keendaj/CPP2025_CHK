c92c9d959a812746ba874c4bf4de8dea7efb5a8a
Keendaj <167191489+Keendaj@users.noreply.github.com>
2025-12-17 00:14:32 +0300

    Update wrapper.h

--- source/wrapper.h
+++ source/wrapper.h
@@ -10,21 +10,21 @@
 #include <any>
 #include <utility>
 #include <memory>
+#include <exception>
 #include <iostream>
 
-
 #define DEBUG_WRAPPER 1
-
 
 template<class Class, class Function>
 class wrapper
 {
     static_assert(std::is_class<Class>::value,
                   "Class must be a class type");
-    static_assert(std::is_member_function_pointer_v<Function>, "Function must be a member function pointer");
+    static_assert(std::is_member_function_pointer_v<Function>,
+         "Function must be a member function pointer");
 
 private:
-    template<typename>
+    template<typename T>
     struct methodTraits;
 
     template<typename C, typename R, typename... Args>
@@ -33,49 +33,55 @@ private:
         using returnType = R;
         using argsTuple = std::tuple<Args...>;
         static constexpr size_t argCount = sizeof...(Args);
-         using argTypes = std::tuple<Args...>;
+        using argTypes = std::tuple<Args...>;
+        
+        template<size_t Index>
+        using argType = typename std::tuple_element<Index, std::tuple<Args...>>::type;
     };
 
     using traits = methodTraits<Function>;
-    using argsTuple = typename traits::argsTuple;
-
 
     std::shared_ptr<Class> _instance;
     Function _function;
-    std::vector<int> defaultArgs;
+    std::vector<std::any> defaultArgs;
     std::vector<std::string> argNames;
     std::unordered_map<std::string, size_t> argsIndexMap;
 
-    template<std::size_t... I>
-    void makeDefaultTuple(const std::vector<std::pair<std::string,int>>& defaults, std::index_sequence<I...>)
+    void makeDefaultTuple(const std::vector<std::pair<std::string,std::any>>& defaults)
     {
         defaultArgs.resize(defaults.size());
-        for (size_t i = 0; i < defaults.size(); ++i)
+        for (size_t i = 0; i < defaults.size(); ++i){
             defaultArgs[i] = defaults[i].second;
+        }
     }
 
-
-    int selectArg(size_t index, const std::unordered_map<std::string,int>& overrides) const
+    template<std::size_t I>
+    auto selectArg(const std::unordered_map<std::string,std::any>& overrides) const
     {
-        const std::string& name = argNames[index];
+        using ArgType = typename traits::argType<I>;
+
+        const std::string& name = argNames[I];
         auto it = overrides.find(name);
+        
         if (it != overrides.end()) {
-            return it->second;
+            return std::any_cast<ArgType>(it->second);
         }
-        return defaultArgs[index]; 
+
+        return std::any_cast<ArgType>(defaultArgs[I]);
     }
 
     template<std::size_t... I>
-    argsTuple makeArgsTuple(const std::unordered_map<std::string,int>& overrides, std::index_sequence<I...>) const
+    typename traits::argsTuple makeArgsTuple(const std::unordered_map<std::string, std::any>& overrides, std::index_sequence<I...>) const
     {
-        return std::make_tuple(selectArg(I, overrides)...);
+        return std::make_tuple(selectArg<I>(overrides)...);
     }
 
-    wrapper(std::shared_ptr<Class> instance, Function function, const std::vector<std::pair<std::string,int>>& defaults)
+    wrapper(std::shared_ptr<Class> instance, Function function, const std::vector<std::pair<std::string,std::any>>& defaults)
         : _instance(instance), _function(function)
     {
-        if (defaults.size() != traits::argCount)
-            throw std::invalid_argument("Default arguments size must match function parameter count");
+        if (defaults.size() != traits::argCount){
+            throw std::runtime_error("Default arguments size must match function parameter count");   
+        }
 
         argNames.resize(traits::argCount);
         for (size_t i = 0; i < defaults.size(); ++i)
@@ -84,21 +90,23 @@ private:
             argsIndexMap[argNames[i]] = i;
         }
 
-        makeDefaultTuple(defaults, std::make_index_sequence<traits::argCount>{});
+        makeDefaultTuple(defaults);
     }
 
     template<typename C2, typename F2>
-    friend auto makeWrapper(std::shared_ptr<C2> instance, F2 function, const std::vector<std::pair<std::string,int>>& defaultArgs);
+    friend auto makeWrapper(std::shared_ptr<C2> instance,
+                            F2 function, 
+                            const std::vector<std::pair<std::string, std::any>>& defaultArgs);
 
 public:
-    typename traits::returnType execute(const std::vector<std::pair<std::string,int>>& overridesVec)
+    typename traits::returnType execute(const std::vector<std::pair<std::string, std::any>>& overridesVec)
     {
-        std::unordered_map<std::string,int> overrides;
+        std::unordered_map<std::string, std::any> overrides;
         for (const auto& p : overridesVec) {
             overrides[p.first] = p.second;
         }
 
-        argsTuple argsTupleFinal = makeArgsTuple(overrides, std::make_index_sequence<traits::argCount>{});
+        typename traits::argsTuple argsTupleFinal = makeArgsTuple(overrides, std::make_index_sequence<traits::argCount>{});
 
         return std::apply([this](auto&&... unpacked){
             return std::invoke(_function, _instance.get(), unpacked...);
@@ -109,7 +117,7 @@ public:
 template<typename C, typename F>
 auto makeWrapper(std::shared_ptr<C> instance,
                 F function, 
-                const std::vector<std::pair<std::string, int>>& defaultArgs)
+                const std::vector<std::pair<std::string, std::any>>& defaultArgs)
 {
     return wrapper<C,F>(instance, function, defaultArgs);
 }
